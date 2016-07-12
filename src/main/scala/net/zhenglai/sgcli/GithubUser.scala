98b0ddaa60264d777e31e94c5359bdd6d12d1bcd
Zhenglai Zhang <zhenglaizhang@hotmail.com>
2016-07-12 20:56:00 +0800

    Add auth token support

--- src/main/scala/net/zhenglai/sgcli/GithubUser.scala
+++ src/main/scala/net/zhenglai/sgcli/GithubUser.scala
@@ -3,11 +3,11 @@ package net.zhenglai.sgcli
 import org.json4s.native.JsonMethods._
 import org.json4s.{DefaultFormats, _}
 
-import scala.concurrent.{Await, Future}
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
-import scala.io.Source
+import scala.concurrent.{Await, Future}
 import scala.util.{Failure, Success}
+import scalaj.http.Http
 
 /**
   * Created by Zhenglai on 7/12/16.
@@ -18,6 +18,10 @@ case class User(id:Long, userName: String)
 object GithubUser {
   implicit val formats = DefaultFormats
 
+  lazy val token:Option[String] = sys.env.get("GHTOKEN") orElse {
+    println("No token found: trying to continue without authentication")
+    None
+  }
 
   def extractUser(jsonResp: JValue): User = {
     val transformed = jsonResp.transformField {
@@ -27,7 +31,13 @@ object GithubUser {
   }
 
   def fetchFromUrl(url: String): Future[User] = {
-    val resp = Future { Source.fromURL(url).mkString }
+    val baseRequest = Http(url)
+    // TODO parse status code to give better error message if possible
+    val request = token match {
+      case Some(t) => baseRequest.header("Authorization", s"token $t")
+      case None => baseRequest
+    }
+    val resp = Future { request.asString.body }
     val parsedResp = resp map { parse(_)}
     parsedResp map extractUser
   }
